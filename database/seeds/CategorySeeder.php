2da337c89f3ad8ea7cb7c4698301c0bd14fcc76d
Muhamad Adam 84 <muhamadadam20@gmail.com>
2020-02-11 20:49:03 +0700

    fix

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $cat = new Category;
+        $cat->category = "News";
+        $cat->slug = "news";
+        $cat->save();
+
+        $cat = new Category;
+        $cat->category = "Articles";
+        $cat->slug = "articles";
+        $cat->save();
+
+        $cat = new Category;
+        $cat->category = "Prestasi";
+        $cat->slug = "prestasi";
+        $cat->save();
+    }
+}
