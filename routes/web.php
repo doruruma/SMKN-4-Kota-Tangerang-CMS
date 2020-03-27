0af7c3d6b789041b3bdb7779438da215cff882b2
Elwandy Tirtana Deriansyah 72 <eltirder0@gmail.com>
2020-03-27 09:06:38 +0700

    add store and delete image

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/gallery', 'GalleryController@index');
+Route::post('/admin/gallery', 'GalleryController@storeImage');
+Route::delete('/admin/gallery', 'GalleryController@deleteFile');
